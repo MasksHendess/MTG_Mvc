e286f4e995cf41f2ebae248ebdf7d416d170e12f
Max <maxhendess@gmail.com>
2020-09-06 14:31:24 +0200

    new DbContext Configuration

--- MTG_Mvc/Startup.cs
+++ MTG_Mvc/Startup.cs
@@ -33,9 +33,7 @@ namespace MTG_Mvc
             services.AddRazorPages();
             services.AddControllers();
             services.AddTransient<JsonFileProductService>();
-            //services.AddDbContext<DbContext>(options =>
-            //options.UseSqlServer(@"Server=.;Database=MTG_Mvc;User=sa;Password=change_this_password;"));
-            services.AddDbContext<SqlDbContext>();
+            services.AddDbContext<SqlDbContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddScoped<decklist>();
             services.AddScoped<decklistService>(); 
             services.AddScoped<decklistRepository>();
